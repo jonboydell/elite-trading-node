b3293086240dc4e10372d7f19a8957f74242fbc3
jonboydell <jonboydell@hotmail.com>
2015-09-10 17:35:43 +0100

    Added crude CLI

--- multiple_destination.js
+++ multiple_destination.js
@@ -1,22 +1,55 @@
 var fs = require('fs');
+var readline = require('readline');
+var rl = readline.createInterface(process.stdin, process.stdout);
+var markets = load_markets("./market");
 
-var buy_location;
-var sell_location;
 var cargo = 0;
-var max_outlay = 10000000;
+var capital = 0;
 
-process.argv.forEach(function (val, index, array) {
-    if (index == 2) {
-        buy_location = val;
+rl.on('line', function(line) {
+    if (line == "all") {
+        for (location of markets.keys()) {
+            var recommendations = get_recommended_buys_at_location(location, markets, cargo, capital);
+            for (recommendation of recommendations) {
+                console.log("%s->%s %s (b:%d, s:%d)",
+                        recommendation.location,
+                        recommendation.destination,
+                        recommendation.commodity,
+                        recommendation.buy,
+                        recommendation.sell,
+                        recommendation.buy * 6,
+                        recommendation.sell * 6,
+                        recommendation.max_price_delta * 6
+                );
+            }
+        }
+    } else if (line.startsWith("l")) {
+        var location = line.slice(line.indexOf(" ")+1);
+        var recommendations = get_recommended_buys_at_location(location, markets, cargo, capital);
+        for (recommendation of recommendations) {
+            console.log("%s->%s %s (b:%d, s:%d)",
+                    recommendation.location,
+                    recommendation.destination,
+                    recommendation.commodity,
+                    recommendation.buy,
+                    recommendation.sell,
+                    recommendation.buy * 6,
+                    recommendation.sell * 6,
+                    recommendation.max_price_delta * 6
+            );
+        }
+    } else if (line.startsWith("cargo")) {
+        cargo = line.slice(line.indexOf(" ")+1);
+        console.log("cargo:"+cargo);
+    } else if (line.startsWith("capital")) {
+        capital = line.slice(line.indexOf(" ")+1);
+        console.log("capital:"+capital);
+    } else if (line.startsWith("status")) {
+        console.log("cargo:"+cargo);
+        console.log("capital:"+capital);
     }
+}).on('close',function() {
 
-    if (index == 3) {
-        cargo = val;
-    }
-
-    if (index == 4) {
-        max_outlay = val;
-    }
 });
 
 function load_markets(market_directory) {
@@ -47,42 +80,6 @@ function parse_prices_to_map(prices) {
         }
     }
     return price_map;
-}
-
-function get_prices_for_market(market_name, markets) {
-    return markets.get(market_name);
-}
-
-function get_buys(prices) {
-    var buys = [];
-    for (var x = 0; x < prices.length; x++) {
-        var buy_price = prices[x];
-        if (buy_price.buy && buy_price.buy > 0) {
-            buys.push(buy_price);
-        }
-    }
-    return buys;
-}
-
-function get_sells(prices) {
-    var sells = [];
-    for (var x = 0; x < prices.length; x++) {
-        var sell_price = prices[x];
-        if (sell_price.sell && sell_price.sell > 0) {
-            sells.push(sell_price);
-        }
-    }
-    return sells;
-}
-
-function get_destinations(current_location, markets) {
-    var destinations = [];
-    for (market_name of markets.keys()) {
-        if (current_location != market_name) {
-            destinations.push(market_name);
-        }
-    }
-    return destinations;
 }
 
 function get_recommended_buys_at_location(location, markets, max_cargo, max_buy_value) {
@@ -120,69 +117,7 @@ function get_recommended_buys_at_location(location, markets, max_cargo, max_buy_
                 }
             }
             max_buys.push(max_buy);
-            //console.log("%s %s %s %d", location, destination, max_buy.commodity, max_price_delta * max_cargo);
         }
     }
     return max_buys;
 }
-
-var markets = load_markets("./market");
-for (location of markets.keys()) {
-    var recommendations = get_recommended_buys_at_location(location, markets, 6, 15000);
-    for (recommendation of recommendations) {
-        console.log("%s->%s %s (b:%d, s:%d)",
-                recommendation.location,
-                recommendation.destination,
-                recommendation.commodity,
-                recommendation.buy,
-                recommendation.sell,
-                recommendation.buy * 6,
-                recommendation.sell * 6,
-                recommendation.max_price_delta * 6
-        );
-    }
-}
-
-
-/**current_location_prices = get_prices_for_market(buy_location, markets);
-current_location_buys = get_buys(current_location_prices);
-
-var destinations = get_destinations(buy_location, markets);
-
-var max_buy;
-var max_profit = 0;
-var location;
-
-for (destination of destinations) {
-    var destination_sells = get_sells(get_prices_for_market(destination, markets));
-    console.log(destination);
-    console.log("---");
-    for (buy_price of current_location_buys) {
-        for (sell_price of destination_sells) {
-            if (buy_price.name == sell_price.name) {
-                var commodity = buy_price.name;
-                var buy = buy_price.buy;
-                var sell = sell_price.sell;
-                var delta = sell - buy;
-                if (delta > 0) {
-                    var output_string = commodity + " " + buy + " " + sell + " " + delta;
-                    if (cargo > 0) {
-                        var total_sell_value = cargo * sell;
-                        var total_buy_value = cargo * buy;
-                        var total_profit = total_sell_value - total_buy_value;
-                        if (total_profit > max_profit && max_outlay > total_buy_value) {
-                            location = destination;
-                            max_buy = buy_price;
-                            max_profit = total_profit;
-                        }
-                        output_string = output_string + " " + total_buy_value + " " + total_sell_value + " profit=" + total_profit;
-                    }
-                    console.log(output_string);
-                }
-            }
-        }
-    }
-}
-console.log(location + " " + max_buy.name + " " + max_profit);
-
-**/
