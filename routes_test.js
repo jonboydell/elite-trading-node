0ede8229314eb0788306d006f61a06b37c945f38
jonboydell <jonboydell@hotmail.com>
2015-09-18 10:53:39 +0100

    corrected the routing function

--- routes_test.js
+++ routes_test.js
@@ -1,78 +1,70 @@
 var routes = require("./routes.json");
 
-// defaults
-
-var location = "kini";
-var destination = "frigaha";
-
-// read in args
+var start;
+var end;
 
 process.argv.forEach(function (val, index, array) {
     if (index == 2) {
-        location = val;
+        start = val;
     }
 
     if (index == 3) {
-        destination = val;
+        end = val;
     }
 });
 
-/// main loop
+var route_map = new Map();
 
-var route_table = load_routes(routes);
-if (route_table.get(location) && route_table.get(destination)) {
-    console.log(traverse(location, destination, [location], 0));
-} else {
-    console.log("NOKAY");
+for (route_pair of routes) {
+    var a = route_pair[0];
+    var b = route_pair[1];
+
+    if (route_map.has(a)) {
+        var exits = route_map.get(a);
+        exits.push(b);
+    } else {
+        var exits = [];
+        exits.push(b);
+        route_map.set(a, exits);
+    }
+
+    if (route_map.has(b)) {
+        var exits = route_map.get(b);
+        exits.push(a);
+    } else {
+        var exits = [];
+        exits.push(a);
+        route_map.set(b, exits);
+    }
 }
 
-// private functions from here
+var y = 0;
+var p = 0;
 
-function load_routes(routes) {
-    var route_table = new Map();
-    for (route_pair of routes) {
-        var a = route_pair[0];
-        var b = route_pair[1];
-
-        if (route_table.has(a)) {
-            route_table.get(a).push(b);
-        } else {
-            var c = [];
-            c.push(b);
-            route_table.set(a, c);
+function route(start, end, route_map, route_list) {
+    y++;
+    var systems = route_map.get(start);
+    if (systems) {
+        if (systems.indexOf(end) > -1) {
+            return end;
         }
+        route_map.delete(start);
 
-        if (route_table.has(b)) {
-            route_table.get(b).push(a);
-        } else {
-            var c = [];
-            c.push(a);
-            route_table.set(b, c);
+        for (var i = 0; i < systems.length; i++) {
+            var x = route(systems[i], end, route_map, route_list);
+            if (x) {
+                route_list.push(systems[i]);
+                p++;
+                return x;
+            }
         }
     }
-    return route_table;
 }
 
-function traverse(location, destination, traversed, count) {
-    var nodes = route_table.get(location);
-    for (d of traversed) {
-        if (nodes.indexOf(d) > -1) {
-            nodes.splice(nodes.indexOf(d), 1);
-        }
-    }
-
-    if (nodes.indexOf(destination) > -1) {
-        console.log("%s->%s", location, destination);
-        return count+1;
-    }
-
-    for (n of nodes) {
-        console.log("%s->%s", location, n);
-        if (n == destination) {
-            return count;
-        }
-        traversed.push(n);
-        count = count + 1;
-        return traverse(n, destination, traversed, count);
-    }
+if (start && end && route_map.get(start) && route_map.get(end)) {
+    var route_list = [];
+    console.log("result ", route(start, end, route_map, route_list));
+    console.log("iterations ", y);
+    console.log("good iterations ", p);
+    console.log(route_list);
 }
